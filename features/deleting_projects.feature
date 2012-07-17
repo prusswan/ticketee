f0940dbd3a32fc27f70f3d5369ddf722136b896b
prusswan <prusswan@gmail.com>
2012-07-17 13:16:38 +0800

    Implemented delete functionality for projects

---
+++ features/deleting_projects.feature
@@ -0,0 +1,15 @@
+Feature: Deleting projects
+  In order to remove needless projects
+  As a project manager
+  I want to make them disappear
+
+  Scenario: Deleting a project
+    Given there is a project called "TextMate 2"
+    And I am on the homepage
+    # When I follow "TextMate 2"
+    # And I follow "Delete Project"
+    When I delete the project called "TextMate 2"
+    # Then I should see "Project has been deleted."
+    Then I should be informed that the project has been deleted
+    # And I should not see "TextMate 2"
+    And I should not be on the project page for "TextMate 2"
--- features/step_definitions/project_steps.rb
+++ features/step_definitions/project_steps.rb
@@ -12,9 +12,14 @@ Then /^I should be informed that the project has(|\snot) been ([^\"]*)$/ do |fla
   page.should have_content("Project has#{flag} been #{action}.")
 end
 
-Then /^I should be on the project page for "([^"]*)"$/ do |name|
-  current_path.should == project_path(Project.find_by_name!(name))
-  page.should have_content("#{name} - Projects - Ticketee")
+Then /^I should(|\snot) be on the project page for "([^"]*)"$/ do |flag, name|
+  if flag.length == 0
+    current_path.should == project_path(Project.find_by_name!(name))
+    page.should have_content("#{name} - Projects - Ticketee")
+  else
+    current_path.should == projects_path
+    page.should_not have_content("#{name} - Projects - Ticketee")
+  end
 end
 
 When /^I try to create a project without a name$/ do
@@ -39,4 +44,9 @@ When /^I rename the "([^"]*)" project to "([^"]*)"$/ do |name, new_name|
   click_link 'Edit Project'
   fill_in :Name, with: new_name
   click_button 'Update Project'
+end
+
+When /^I delete the project called "([^"]*)"$/ do |name|
+  click_link name
+  click_link 'Delete Project'
 end
