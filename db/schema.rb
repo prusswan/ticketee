b45ad544495a3fedeea42d82fe573842a7c7f907
prusswan <prusswan@gmail.com>
2012-07-23 21:20:25 +0800

    Users can tag tickets upon creation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120723104756) do
+ActiveRecord::Schema.define(:version => 20120723130954) do
 
   create_table "assets", :force => true do |t|
     t.string   "asset_file_name"
@@ -53,6 +53,15 @@ ActiveRecord::Schema.define(:version => 20120723104756) do
     t.string  "color"
     t.string  "background"
     t.boolean "default",    :default => false
+  end
+
+  create_table "tags", :force => true do |t|
+    t.string "name"
+  end
+
+  create_table "tags_tickets", :id => false, :force => true do |t|
+    t.integer "tag_id"
+    t.integer "ticket_id"
   end
 
   create_table "tickets", :force => true do |t|
