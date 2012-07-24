14debbd97a993aeb03485ba91cacd842a6d057c4
prusswan <prusswan@gmail.com>
2012-07-24 17:18:24 +0800

    Set up application to connect to Gmail to send emails

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ gem 'jquery-rails'
 
 group :test, :development do
   gem 'rspec-rails', '~> 2.5'
+  gem 'gmail'
 end
 
 group :test do
--- features/step_definitions/app_email_steps.rb
+++ features/step_definitions/app_email_steps.rb
@@ -7,3 +7,24 @@ Then /^there should be a part with content type "([^"]*)"$/ do |content_type |
     p.content_type.include?(content_type)
   end.should_not be_nil
 end
+
+Given /^Action Mailer delivers via SMTP$/ do
+  ActionMailer::Base.delivery_method = :smtp
+end
+
+When /^I log into gmail with:$/ do |table|
+  details = table.hashes.first
+  @gmail = Gmail.connect(details["username"], GMAIL_PASSWORD)
+end
+
+Then /^there should be an email from Ticketee in my inbox$/ do
+  sender = ActionMailer::Base.smtp_settings[:user_name]
+  @mails = @gmail.inbox.find(:unread, from: "#{sender}")
+  @mails.each do |mail|
+    if mail.subject =~ /^\[ticketee\]/
+      mail.delete!
+      @received_mail = true
+    end
+  end
+  @received_mail.should be_true
+end
