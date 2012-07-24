3f59bb7707b54ab9550bc3dbab49984c88d013bf
prusswan <prusswan@gmail.com>
2012-07-24 23:34:59 +0800

    Added rate limiting to the V1 API

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,10 @@ class User < ActiveRecord::Base
 
   before_save :ensure_authentication_token
 
+  def self.reset_request_count!
+    update_all("request_count = 0", "request_count > 0")
+  end
+
   def to_s
     "#{email} (#{admin? ? "Admin" : "User"})"
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120724114936) do
+ActiveRecord::Schema.define(:version => 20120724152631) do
 
   create_table "assets", :force => true do |t|
     t.string   "asset_file_name"
@@ -100,6 +100,7 @@ ActiveRecord::Schema.define(:version => 20120724114936) do
     t.datetime "confirmation_sent_at"
     t.boolean  "admin",                  :default => false
     t.string   "authentication_token"
+    t.integer  "request_count",          :default => 0
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
