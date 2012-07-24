48512d1e2b325955fcb3edb4a77d1c8848b2efa0
prusswan <prusswan@gmail.com>
2012-07-24 20:45:01 +0800

    Implemented token-based authentication API base

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Ticketee::Application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :projects
+    end
+  end
 
   devise_for :users, :controllers => { registrations: "registrations" }
   get '/awaiting_confirmation',
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120723230826) do
+ActiveRecord::Schema.define(:version => 20120724114936) do
 
   create_table "assets", :force => true do |t|
     t.string   "asset_file_name"
@@ -99,6 +99,7 @@ ActiveRecord::Schema.define(:version => 20120723230826) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.boolean  "admin",                  :default => false
+    t.string   "authentication_token"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
